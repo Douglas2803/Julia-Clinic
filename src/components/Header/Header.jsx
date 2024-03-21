2969f370b19c199226b0723c29c5c8aafc1c3883
Douglas <douglasgia@hotmail.com>
2024-03-21 14:58:53 -0300

    Header navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
+import { MdOutlineClose } from "react-icons/md";
 import { GiHamburgerMenu } from "react-icons/gi";
-
-import './style.scss'
+import "./style.scss";
 
 const Header = () => {
+  const [sidebarVisible, setSidebarVisible] = useState(false);
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+  };
+
   return (
     <header>
       <nav>
-        <a href="#">Logogit push origin main
- Clinica</a>
-        <GiHamburgerMenu className="nav-icon" />
+        <a href="#">Logo da Clinica Aqui</a>
+        <button onClick={toggleSidebar} className="icon-button">
+          <GiHamburgerMenu className="icon" />
+        </button>
       </nav>
+      {sidebarVisible && (
+        <div className="sidebar">
+          <nav className="nav-sidebar">
+            <button onClick={toggleSidebar} className="close-button">
+              <MdOutlineClose className="close" />
+            </button>
+            <a href="#">Home</a>
+            <a href="#">Tratamentos</a>
+            <a href="#">Resultados</a>
+          </nav>
+        </div>
+      )}
     </header>
   );
-}
+};
 
-export default Header
+export default Header;
