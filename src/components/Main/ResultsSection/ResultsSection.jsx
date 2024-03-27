87d0510a38a3e082a3ec00a548f825a1a45884ee
Douglas <douglasgia@hotmail.com>
2024-03-27 08:30:16 -0300

    slider-fotos

--- src/components/Main/ResultsSection/ResultsSection.jsx
+++ src/components/Main/ResultsSection/ResultsSection.jsx
@@ -1,6 +1,10 @@
 import React from "react";
-import Avatar from "../../../img/clinica10.jpeg";
-import Avatar2 from "../../../img/clinica11.jpeg";
+import Avatar from "../../../img/paciente1-a.jpeg";
+import Avatar2 from "../../../img/paciente1-b.jpeg";
+import Avatar3 from "../../../img/paciente2-a.jpeg";
+import Avatar4 from "../../../img/paciente2-b.jpeg";
+import Avatar5 from "../../../img/paciente3-a.jpeg";
+import Avatar6 from "../../../img/paciente3-b.jpeg";
 import {
   ReactCompareSlider,
   ReactCompareSliderImage,
@@ -12,24 +16,67 @@ function ResultsSection() {
   return (
     <>
       <section className="result">
-        <ReactCompareSlider
-          itemOne={
-            <ReactCompareSliderImage
-              src={Avatar}
-              srcSet={Avatar}
-              alt="Image one"
-              className="slider"
-            />
-          }
-          itemTwo={
-            <ReactCompareSliderImage
-              src={Avatar2}
-              srcSet={Avatar2}
-              alt="Image two"
-              className="slider"
-            />
-          }
-        />
+        <h1>Antes e Depois</h1>
+        <div className="slider">
+          <ReactCompareSlider
+            itemOne={
+              <ReactCompareSliderImage
+                src={Avatar}
+                srcSet={Avatar}
+                alt="Image one"
+                className="slider"
+              />
+            }
+            itemTwo={
+              <ReactCompareSliderImage
+                src={Avatar2}
+                srcSet={Avatar2}
+                alt="Image two"
+                className="slider"
+              />
+            }
+          />
+        </div>
+        <div className="slider">
+          <ReactCompareSlider
+            itemOne={
+              <ReactCompareSliderImage
+                src={Avatar3}
+                srcSet={Avatar3}
+                alt="Image one"
+                className="slider"
+              />
+            }
+            itemTwo={
+              <ReactCompareSliderImage
+                src={Avatar4}
+                srcSet={Avatar4}
+                alt="Image two"
+                className="slider"
+              />
+            }
+          />
+        </div>
+        <div className="slider">
+          <ReactCompareSlider
+            itemOne={
+              <ReactCompareSliderImage
+                src={Avatar5}
+                srcSet={Avatar5}
+                alt="Image one"
+                className="slider"
+              />
+            }
+            itemTwo={
+              <ReactCompareSliderImage
+                src={Avatar6}
+                srcSet={Avatar6}
+                alt="Image two"
+                className="slider"
+              />
+            }
+          />
+        </div>
       </section>
     </>
   );
