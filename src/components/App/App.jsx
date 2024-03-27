ae959e6e4fd7984e43963ec5946ce5ccff72a685
Douglas <douglasgia@hotmail.com>
2024-03-27 13:51:33 -0300

    Result-Section-Resolve

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../Header/Header'
 import Main from '../Main/Main'
+import Footer from '../Footer/Footer'
 import './App.scss'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   )
 }
